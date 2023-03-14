697b0f4a79b3d9207160b7abd52907350496ab65
Andrey_Lozovoy <lozovoya@gmail.com>
2023-03-14 22:49:50 +0500

    with action

--- deploy/tf/main.tf
+++ deploy/tf/main.tf
@@ -22,7 +22,7 @@ terraform {
 provider "yandex" {
   zone = "ru-central1-a"
   #service_account_key_file = "/Users/Andrey_Lozovoy/.yc/key.json"
-  service_account_key_file = "/Users/Andrey_Lozovoy/.yc/key.json"
+  service_account_key_file = "$YA_KEY"
   cloud_id                 = "$b1gjtddi41p742jcni7p"
 }
 
